17ded957d65b5d04c29098ee989fab14604322ac
Ramvijay Yadav <102288041+Ramraw@users.noreply.github.com>
2022-11-17 21:08:04 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import pyttsx3
+import PyPDF2
+book = open('oop.pdf', 'rb')
+pdf_Reader = PyPDF2.PdfFileReader(book)
+pages = pdf_Reader.numPages
+
+speaker = pyttsx3.init()
+for num in range(7, pages):
+    page = pdfReader.getPage(num)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
